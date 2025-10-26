be9fd2ab0100a6621f28de697775c2e6dc6bf659
devabratasahoo07 <64681169+devabratasahoo07@users.noreply.github.com>
2025-10-26 16:59:23 +0530

    Enhance Addtocarrt.spec.js with additional tests
    Added multiple test cases for add to cart functionality.

--- tests/add tocart/Addtocarrt.spec.js
+++ tests/add tocart/Addtocarrt.spec.js
@@ -5,3 +5,18 @@ test("Add to cart", async ({ page }) => {
   await page.click("text=Add to cart");
   await page.waitForSelector("text=Item added to cart");
 });
+test("1st test",async({page})=>{
+
+})
+test("2nd test",async({page})=>{
+
+})
+test("3rd test",async({page})=>{
+
+})
+test("4th test",async({page})=>{
+
+})
+test("5th test",async({page})=>{
+
+})
